66856ae32ef0e0b1d88866f214afa740060c644d
leoohyama <leo.ohyama@ufl.edu>
2021-04-15 13:53:04 -0400

    get rid of script and edit graphics script

--- Scripts_Analysis/Graph_script.R
+++ Scripts_Analysis/Graph_script.R
@@ -1,4 +1,4 @@
-####graphics
+####graphics for the study
 library(tidyverse)
 library(patchwork)
 library(wesanderson)
@@ -16,13 +16,8 @@ df<-df %>%
   mutate(range = (Max_A - Min_A)) %>%
   filter(!Study.ID == 29)
 
-ggplot(df) + geom_point(aes(x=df$Range.order.of.magnitude., y = Z))
 
-#c,z,and R2
-ggplot(df) + geom_point(aes(x = Z, y = C, size =R2, color = abs(Lat))) +
-  scale_color_distiller(palette = "Spectral") +scale_size_continuous(range = c(1,14)) 
-
-#categorical vraibles with z-value
+#categorical variables with z-value
 ecoregions<-df %>%
   ggplot(.) + 
   geom_boxplot(aes(x = Ecoregion, y = Z, fill = Ecoregion)) +
@@ -36,6 +31,7 @@ ecoregions<-df %>%
         axis.title.x = element_blank(),
         axis.title.y = element_text(size = 14,angle = 0, face = 'bold', vjust = 0.5))
 
+#By Sar_type
 SAR_tyype<-df %>%
   ggplot(.) + 
   geom_boxplot(aes(x = SAR_type, y = Z),fill = "grey") +
@@ -48,6 +44,8 @@ SAR_tyype<-df %>%
 df$Island_Type2<-as.factor(df$Island_Type)
 levels(df$Island_Type2)<-c("Continental\n(true island)", "Mainland Islands",
                            "Oceanic\n(true island)")
+
+#by island type
 island_type<-df %>%
   ggplot(.) + 
   geom_boxplot(aes(x = Island_Type2, y = Z, fill = Island_Type2)) +
@@ -123,49 +121,4 @@ ggsave('figx.jpg',
        height = 5, 
        width = 6,
        dpi = 500)
-
-geom_rect(mapping=aes(xmin=0, xmax=3100, ymin=-1, ymax=0), fill="grey", alpha=0.5)+ #r2
-  geom_rect(mapping=aes(xmin=0, xmax=3100, ymin=0, ymax=1), fill="cornsilk2", alpha=0.5) + #Z
-
-fancy$OM<-as.numeric(as.character(fancy$Range.order.of.magnitude.))  
-class(fancy$Range.order.of.magnitude.)
-figure_y<-ggplot(data = fancy) + 
-  geom_bar(stat= "identity", aes(x = (Range.order.of.magnitude.), y = Z, fill = Ecoregion), 
-           position = position_dodge(width = 1), width = 0.5) +
-  geom_point(aes(x = (Range.order.of.magnitude.), y = Z, color = Ecoregion),
-             position = position_dodge(width = 1)) +
-  geom_hline(yintercept = 0) +
-  labs(x = "Mean Annual Precipitation", y = "", fill = "Biogeographic \nRegion",
-       color = "Biogeographic \nRegion") +
-  scale_y_continuous(labels = abs(pretty(fancy$Z))) +
-  scale_fill_viridis_d(option = "D") +
-  scale_color_viridis_d(option = "D") +
-  coord_flip(xlim = c(0,3100), ylim = c(-1,1),expand = FALSE) +
-  annotate("text", x = 2900, y = 0.5,size = 6, label = "Z-value")+
-  annotate("text", x = 2900, y = -0.5,size = 6, label = lb1, parse =T) +
-  theme(axis.title.y = element_text(face= "bold", size =10),
-        legend.title = element_text(face = "bold", size = 10))
-
-
-
-
-
-SAR_tyype<-df %>%
-  ggplot(.) + 
-  geom_boxplot(aes(x = SAR_type, y = Z),fill = "dodgerblue", color = "white", width = 0.7) +
-  scale_fill_viridis_d(option = "E") +
-  theme_bw() +
-  theme(
-    panel.grid.major = element_blank(), 
-    panel.grid.minor = element_blank(),
-    panel.background = element_rect(fill = "transparent",colour = NA),
-    plot.background = element_rect(fill = "transparent",colour = NA),
-    legend.title = element_blank(),
-    axis.title.y = element_text(size = 22,angle = 0, face = 'bold', vjust = 0.5, color = "white"),
-    axis.title.x = element_blank(),
-    axis.text = element_text(size = 22, face = "bold", color = "white"),
-    axis.line = element_line(color = "white", 
-                             size = 1, linetype = "solid")
-  )
-
 
